580cafe52114ec3d77ab5116b4a9d57f28ccbfd8
Gabriel_Stundner <gabriel.stundner@dell.com>
2025-10-29 17:49:22 -0300

    Feature(#1) Stack studies

---
+++ Structure.Queue/Performance.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+using System.Collections;
+
+namespace StackExample
+{
+    public class Performance
+    {
+        public void TestBasicStackPerformance()
+        {
+            const int count = 1000000;
+            Stack stack = new Stack();
+            Stopwatch sw = Stopwatch.StartNew();
+            for (int i = 0; i < count; i++)
+            {
+                stack.Push(i);
+            }
+            for (int i = 0; i < count; i++)
+            {
+                stack.Pop();
+            }
+            sw.Stop();
+            Console.WriteLine($" [Basic Stack] Push and Pop time from {count} values: {sw.ElapsedMilliseconds} ms");
+        }
+
+        public void TestGenericStackPerformance()
+        {
+            const int count = 1000000;
+            Stack<int> stack = new Stack<int>();
+            Stopwatch sw = Stopwatch.StartNew();
+            for (int i = 0; i < count; i++)
+            {
+                stack.Push(i);
+            }
+            for (int i = 0; i < count; i++)
+            {
+                stack.Pop();
+            }
+            sw.Stop();
+            Console.WriteLine($" [Generic Stack] Push and Pop time from {count} values: {sw.ElapsedMilliseconds} ms");
+        }
+
+        
+    }
+}
